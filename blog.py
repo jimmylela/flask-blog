df583e2f3272a91ef53021eca165674e3097ff28
jimmylela <binaryvisionary@gmail.com>
2023-07-29 10:12:20 -0500

    update blog.py

--- blog.py
+++ blog.py
@@ -1,18 +1,24 @@
 ## Python script to create SQLite database for Flask blog app
 ##
-## Created 2023023, Jimmy Lela
-## Updated 2023023, Jimmy Lela
+## Created 20230723, Jimmy Lela
+## Updated 20230729, Jimmy Lela
 
-## import modules
-from flak import Flask, render_template, request, session, flash, redirect, url_for, g
+## import Flask and other classes from flask module
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+
+## import sqlite3 module
 import sqlite3
 
-## DATABASE = 'blog.db'
+## save database name to variable
+DATABASE = 'blog.db'
 
-## define Flask app - __name__ ?? 20230722
+## instantiate a Flask class object, pass it the __name_variable, which 
+## will have the value of this Python file (blog.py), telling Flask
+## to look for other resources in the same directory using all caps variables
+## Why doesn't it have the value of "__main__" if it's ran directly? - 20230729
 app = Flask(__name__)
 
-## pulls in app configuration by looking for UPPERCASE variables - DAFUQ? 20230722
+## pull in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
 ## define function for connecting to database
